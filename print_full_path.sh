aa8aaea9af48f4d9f53206c133cba9afdd94e5d3
Lane Schwartz <dowobeha@gmail.com>
2014-09-03 14:01:38 -0500

    Standardize instructions

--- print_full_path.sh
+++ print_full_path.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+###############################################################################
+# Instructions:
+###############
+#
 # Modify this script so that it prints to standard output 
 #  the full path from the root directory to the current directory
+#
+###############################################################################
 
+
+
+# Students - delete this line and put your work here
--- show_all-number_lines.sh
+++ show_all-number_lines.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+###############################################################################
+# Instructions:
+###############
+#
 # Modify this script so that it accepts text from standard input,
 #  shows non-printing characters use ^ and M- notation (except for LFD and TAB),
 #  displays $ at end of each line,
 #  displays TAB characters as ^I,
 #  and numbers all output lines,
 #  printing the resulting text to standard output
+#
+###############################################################################
+
+
+
+# Students - delete this line and put your work here
 
 
 
